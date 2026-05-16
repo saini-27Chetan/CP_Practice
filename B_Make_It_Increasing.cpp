d6d6e236639ecdf607aa3e00d48780c407cf4644
ChetanSaini <chetan27saini@gmail.com>
2026-05-16 11:13:55 +0530

    16May2026

--- B_Make_It_Increasing.cpp
+++ B_Make_It_Increasing.cpp
@@ -1,30 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define ll long long
 
-void process(){
+void performTask(){
     int n;
     cin>>n;
 
-    vector<long> a(n);
+    vector<ll> a(n);
     for(int i=0; i<n; i++)
         cin>>a[i];
 
-    int i=n-2, count=0;
+    int i=n-2, cnt=0;
     while(i>=0){
         while(a[i]>=a[i+1]){
             a[i] >>= 1;
-            count++;
+            cnt++;
             if(a[i]==0)
                 break;
         }
         if(a[i]==0 && a[i+1]==0){
-            count=-1;
+            cnt=-1;
             break;
         }
         i--;
     }
 
-    cout<<count<<endl;
+    cout<<cnt<<"\n";
     return;
 }
 
@@ -32,7 +33,7 @@ int main(){
     int t;
     cin>>t;
     while(t--){
-        process();
+        performTask();
     }
     return(0);
 }
--- D_Deletive_Editing.cpp
+++ D_Deletive_Editing.cpp
@@ -1,31 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define ll long long
 
-void process(){
+void performTask(){
     string s, t;
     cin>>s>>t;
 
-    int len_t = t.length();
-    int len_s = s.length();
+    int x=t.length();
+    int y=s.length();
     
     vector<int> freq(26, 0);
-	for (int i = 0; i < len_t; i++) 
-		freq[t[i] - 'A']++;    
+	for (int i=0; i<x; i++) 
+		freq[t[i]-'A']++;    
 
-    for(int i=len_s-1; i>=0; i--){
+    for(int i=y-1; i>=0; i--){
         if(freq[s[i]-'A']>0)
             freq[s[i]-'A']--;
         else
             s[i]='.';
     }
 
-    string ans="";
-    for(int i=0; i<len_s; i++){
+    string res="";
+    for(int i=0; i<y; i++){
         if(s[i]!='.')
-            ans+=s[i];
+            res+=s[i];
     }
 
-    if(ans==t)
+    if(res==t)
         cout<<"YES\n";
     else
         cout<<"NO\n";
@@ -36,7 +37,7 @@ int main(){
     int t;
     cin>>t;
     while(t--){
-        process();
+        performTask();
     }
     return(0);
 }
