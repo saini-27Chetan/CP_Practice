faa8ccbf8cecb69e9b049ff5744cda803ef19867
Chetan Saini <142503455+saini-27Chetan@users.noreply.github.com>
2026-06-05 11:08:35 +0530

    05June2026

--- B_Array_merging.cpp
+++ B_Array_merging.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 #define ll long long
 
-void process(){
+void performTask(){
     ll n;
     cin>>n;
-    vector<ll> a(n), b(n);
-    unordered_map<int, int> m_a, m_b;
+    vector<ll> a(n),b(n);
+    unordered_map<int, int> m_a,m_b;
     for(ll i=0; i<n; i++)
         cin>>a[i];
 
@@ -18,35 +18,34 @@ void process(){
         if(a[i]==a[i-1])
             len++;
         else{
-            m_a[a[i-1]] = max(m_a[a[i-1]], len);
+            m_a[a[i-1]]=max(m_a[a[i-1]], len);
             len=1;
         }
     }
-    m_a[a[n-1]] = max(m_a[a[n-1]], len);
+    m_a[a[n-1]]=max(m_a[a[n-1]], len);
     
     len=1;
     for(int i=1; i<n; i++){
         if(b[i]==b[i-1])
             len++;
         else{
-            m_b[b[i-1]] = max(m_b[b[i-1]], len);
+            m_b[b[i-1]]=max(m_b[b[i-1]], len);
             len=1;
         }
     }
-    m_b[b[n-1]] = max(m_b[b[n-1]], len);
+    m_b[b[n-1]]=max(m_b[b[n-1]], len);
 
-    int ans = 0;
-    for(auto &x : m_a)
-        ans = max(ans, x.second);
+    int ans=0;
+    for(auto &x:m_a)
+        ans=max(ans, x.second);
 
-    for(auto &x : m_b)
-        ans = max(ans, x.second);
+    for(auto &x:m_b)
+        ans=max(ans, x.second);
 
-    for(auto &x : m_a){
+    for(auto &x:m_a){
         if(m_b.count(x.first))
-            ans = max(ans, x.second + m_b[x.first]);
+            ans=max(ans, x.second+m_b[x.first]);
     }
-
     cout<<ans<<"\n";
     return;
 }
@@ -55,7 +54,7 @@ int main(){
     int t;
     cin>>t;
     while(t--){
-        process();
+        performTask();
     }
     return(0);
 }
