65b5e8d251433c7ce9f55727155c5674f7f3c610
Chetan Saini <142503455+saini-27Chetan@users.noreply.github.com>
2026-06-17 11:04:55 +0530

    17June2026

--- B_Divan_and_a_New_Project.cpp
+++ B_Divan_and_a_New_Project.cpp
@@ -1,39 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
+#define pll pair<ll, ll>
 
-void process(){
+void performTask(){
     ll n;
     cin>>n;
 
-    vector<pair<ll, ll>> a(n);
+    vector<pll> a(n);
     for(ll i=0; i<n; i++){
         ll x;
         cin>>x;
 
-        a[i] = {x, i+1};
+        a[i]={x, i+1};
     }
 
     sort(a.rbegin(), a.rend());
 
-    vector<ll> sequence(n+1, 0);
+    vector<ll> seq(n+1, 0);
     // Headquarter is placed at idx 0
-    ll ans=0;
+    ll res=0;
     ll x=1;
     for(ll i=0; i<n; i++){
-        sequence[a[i].second] = x;
-        ans += (2 * abs(x) * a[i].first);
+        seq[a[i].second]=x;
+        res+=(2*abs(x)*a[i].first);
 
         if(x<0){
-            x = -x;
-            x += 1;
+            x=-x;
+            x++;
         }
         else
-            x = -x;
+            x=-x;
     }
 
-    cout<<ans<<"\n";
-    for(auto x: sequence)
+    cout<<res<<"\n";
+    for(auto &x: seq)
         cout<<x<<" ";
     cout<<"\n";
     return;
@@ -43,7 +44,7 @@ int main(){
     int t;
     cin>>t;
     while(t--){
-        process();
+        performTask();
     }
     return(0);
 }
