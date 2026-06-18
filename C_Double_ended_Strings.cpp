0e0ddfc3ee93439421045c8497bfc28a7e921538
Chetan Saini <142503455+saini-27Chetan@users.noreply.github.com>
2026-06-18 10:48:06 +0530

    18June2026

--- C_Double_ended_Strings.cpp
+++ C_Double_ended_Strings.cpp
@@ -1,32 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
+#define vvvi vector<vector<vector<int>>>
 
-int helper(string& s1, string& s2, int i, int j, int curr, vector<vector<vector<int>>>& dp){
+int helperFunc(string& s1, string& s2, int i, int j, int curr, vvvi &dp){
     if(i<0 || j<0)
         return curr;
 
     if(dp[i][j][curr]!=-1)
         return dp[i][j][curr];
+
     int same=curr;    
     if(s1[i]==s2[j])
-        same=helper(s1, s2, i-1, j-1, curr+1, dp);
+        same=helperFunc(s1, s2, i-1, j-1, curr+1, dp);
         
-    int first=helper(s1, s2, i-1, j, 0, dp);
-    int second=helper(s1, s2, i, j-1, 0, dp);
+    int f=helperFunc(s1, s2, i-1, j, 0, dp);
+    int s=helperFunc(s1, s2, i, j-1, 0, dp);
 
-    return dp[i][j][curr]=max(same, max(first, second));
+    return dp[i][j][curr]=max(same, max(f, s));
 }
 
-void process(){
+void performTask(){
     string a,b;
     cin>>a>>b;
     
     int n=a.length(), m=b.length();
-    vector<vector<vector<int>>> dp (n, vector<vector<int>> (m, vector<int> (min(n,m)+1, -1)));
-    int maxSub=helper(a,b,n-1,m-1, 0, dp);
-    int ans=n+m-(maxSub<<1);
-    cout<<ans<<"\n";
+    vvvi dp(n, vector<vector<int>> (m, vector<int> (min(n,m)+1, -1)));
+    int maxSub=helperFunc(a,b,n-1,m-1, 0, dp);
+    int res=n+m-(maxSub<<1);
+    cout<<res<<"\n";
     return;
 }
 
@@ -34,7 +36,7 @@ int main(){
     int t;
     cin>>t;
     while(t--){
-        process();
+        performTask();
     }
     return(0);
-}
+}    
