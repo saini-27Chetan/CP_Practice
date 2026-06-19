a84280059b577c180eef7e64baa87a023abb6cdf
Chetan Saini <142503455+saini-27Chetan@users.noreply.github.com>
2026-06-19 10:48:15 +0530

    19June2026

--- A_Add_and_Divide.cpp
+++ A_Add_and_Divide.cpp
@@ -2,26 +2,26 @@
 using namespace std;
 #define ll long long
 
-void process(){
+void performTask(){
     int a,b;
     cin>>a>>b;
 
-    ll ans=LLONG_MAX;
+    ll res=LLONG_MAX;
     for(int add=0; add<32; add++){
-        int B=b+add;
         int A=a;
+        int B=b+add;
         if(B==1)    // As a divide by  b will result in a itself so we will stuck in infinte loop
             continue;
 
-        ll count=add; // Moves spent increasing b
+        ll cnt=add; // Moves spent increasing b
         
         while(A>0){
             A/=B;
-            count++;
+            cnt++;
         }
-        ans=min(ans, count);
+        res=min(res, cnt);
     }
-    cout<<ans<<"\n";
+    cout<<res<<"\n";
     return;
 }
 
@@ -29,7 +29,7 @@ int main(){
     int t;
     cin>>t;
     while(t--){
-        process();
+        performTask();
     }
     return(0);
 }
